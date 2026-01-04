1b19a5a7c14b5e2fd0c7e6bab2a53476ed4ca0c6
EuduardoP <dudupiresrosa@gmail.com>
2026-01-04 19:07:05 -0300

    🚚 Move entites to packages
    #

--- packages/api/src/lib.rs
+++ packages/api/src/lib.rs
@@ -1,9 +1,16 @@
 //! This crate contains all shared fullstack server functions.
-pub mod db;
-use dioxus::prelude::*;
+use dioxus::{fullstack::Json, prelude::*};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct EchoInput {
+    pub message: String,
+}
 
 /// Echo the user input on the server.
 #[post("/api/echo")]
-pub async fn echo(input: String) -> Result<String, ServerFnError> {
+pub async fn echo(Json(input): Json<EchoInput>) -> Result<EchoInput, ServerFnError> {
+    use dioxus::logger::tracing;
+    tracing::info!("Echoing message: {}", input.message);
     Ok(input)
 }
